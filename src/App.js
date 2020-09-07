fed99c05b2630df1e9845e31732de2c59da09d93
Michael Lopez <mlopez@rightslne.com>
2020-09-06 23:24:53 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ function App() {
 React.useEffect(()=>{
   console.log("PLAYA", isPlayer1Turn , checkDiagonal() || checkHortizontal() || checkVertical())
   if(checkDiagonal() || checkHortizontal() || checkVertical()) {
-    const player = isPlayer1Turn?"O":"X" //ternary statement
+    const player = isPlayer1Turn?"X":"0" //ternary statement
     setDisplay("WINNER! " + player); 
     setIsGameOver(true); 
-  } 
+  }
 }, [box1, box2, box3, box4, box5, box6, box7, box8, box9])
 
 const handleClick = (boxFunc, accumulator) => {
@@ -49,57 +49,86 @@ const handleClick = (boxFunc, accumulator) => {
     setDisplay("It's player X's turn");
   } 
   setIsPlayer1Turn(!isPlayer1Turn); 
+  
 } 
+
+// ensures that nulls are not counted as answers
+const nullIsNotAnswer = () => {
+  // this function will return true so that CHECKS can ensure that if is false
+  //row1
+  if(box1== "" && box2 == "" && box3 == "") {
+    return true;
+  }
+  //row2
+if(box4 == "" && box5 == "" && box6 == "") {
+  return true; 
+}
+  //row3
+if (box7 == "" && box8 == ""  && box9 == "") {
+  return true; 
+}
+  //vert1
+if (box1 == "" && box5 == "" && box9=="")  {
+  return true; 
+}
+  //vert2
+if (box3 == "" && box5 == "" && box7 =="") {
+  return true
+} 
+return false; 
+}
+
+nullIsNotAnswer(); 
+
 // CHECKS
 const checkDiagonal = () => {
-  if ( acc > 2 ) {
-    if (box1 !="" && box1 == box5 && box5 == box9) {
-      console.log("We have a winner! " + box1);
-      return true; 
-    } else if (box3 !="" && box3 == box5 && box5 == box7) {
-      console.log("The winner is! " + box3);
-      return true
+  if ( acc > 2  ) {
+
+      if (box1 != box1 && box1 == box5 && box5 == box9 && box1 == box9 && box1 != "") {
+        console.log("We have a winner! " + box1);
+        return true; 
+      } else if (box1 != box1 && box3 == box5 && box5 == box7 && box3 == box7) {
+        console.log("The winner is! " + box3);
+        return true
+      }
     }
-    return false; 
-  }
+
 }
 
 
 const checkHortizontal = () => {
   if (acc > 2) {
-    if(box1 !="" && box1 == box2 && box2 == box3) {
-      console.log(box1 + " WON Row 1");
-      return true; 
-    } else if (box4 !="" && box4 == box5 && box5 == box6) {
-      console.log(box4 + " WON! Row2");
-      return true; 
-    } else if(box7 != "" && box7 == box8 && box8 == box9){
-      console.log(box7 + " WON! Row3"); 
-      return true; 
-    }
+   
+      if(box1 != box1 && box1 == box2 && box2 == box3 && box1 == box3) {
+        console.log(box1 + " WON Row 1");
+        return true; 
+      } else if (box4 != box4 && box4 == box5 && box5 == box6 && box4 == box6) {
+        console.log(box4 + " WON! Row2");
+        return true; 
+      } else if(box7!== box7 && box7 == box8 && box8 == box9 && box7 == box9 ){
+        console.log(box7 + " FACK WON! Row3"); 
+        return true; 
+      }
+
     return false; 
   } 
 }
 
 const checkVertical = () => {
   if (acc > 2) {
-    if(box1 != "" && box1 == box4 && box4 == box7){
-      console.log(box1 + " WON Row 1");
-      return true; 
-    } else if(box2 !="" && box2 == box5 && box5 == box8) {
-      return true; 
-    } else if (box3 !="" && box3 == box6 && box6 == box9) {
-      return true; 
-    } 
+
+      if(box1 != box1 && box1 == box4 && box4 == box7 && box1==box7){
+        console.log(box1 + " WON Row 1");
+        return true; 
+      } else if(box2 != box2 && box2 == box5 && box5 == box8 && box2==box8) {
+        return true; 
+      } else if (box3 != box3 && box3 == box6 && box6 == box9 && box3 == box9) {
+        return true; 
+      } 
+    
     return false;
   }
 }
-  
-  // check row vertically  
-
-// button at bottom shows when someone wins = play again
-// 
-
 
 
   return (
@@ -153,3 +182,4 @@ const styles = {
 }
 
 export default App;  
+
